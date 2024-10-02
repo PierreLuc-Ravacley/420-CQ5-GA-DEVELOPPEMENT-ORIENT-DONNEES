53ef9f650b932a9e14bb8084b5d6ce88848a6c50
PierreLuc-Ravacley <pierreluc.ravacley@gmail.com>
2024-10-02 13:54:07 -0400

    Changement de nom et correction DTO

--- Main/Projet/DTO/chambreDTO.py
+++ Main/Projet/DTO/chambreDTO.py
@@ -10,6 +10,7 @@ class TypeChambreDTO(BaseModel):
     description_chambre : str
 
     def __init__(self, type: TypeChambre):
+        super().__init__(nom_type="", prix_plafond=0.0, prix_plancher=0.0, description_chambre="")
         self.nom_type = type.nom_type
         self.prix_plafond = type.prix_plafond
         self.prix_plancher = type.prix_plancher
@@ -22,11 +23,9 @@ class ChambreDTO(BaseModel):
     type_chambre: str
 
     def __init__(self, chambre: Chambre):
+        super().__init__(numero_chambre=0, disponible_reservation=False, autre_informations="", type_chambre="")
         self.numero_chambre = chambre.numero_chambre
         self.disponible_reservation = chambre.disponible_reservation
         self.autre_informations = chambre.autre_informations
-        self.type_chambre = chambre.type_chambre
+        self.type_chambre = chambre.fk_type_chambre
         
-        
-        
-         
--- Main/Projet/main.py
+++ Main/Projet/main.py
@@ -1,17 +1,9 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from Metier.chambreMetier import creerChambre, creerTypeChambre, getChambreParNumero, ChambreDTO, TypeChambreDTO
 
 app = FastAPI()
 
-# Add CORS middleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # You can specify the origins here (e.g., "http://localhost:3000")
-    allow_credentials=True,
-    allow_methods=["*"],  # You can specify which methods are allowed
-    allow_headers=["*"],  # You can specify which headers are allowed
-)
+
     
 @app.get("/chambres/{no_chambre}")
 def read_item(no_chambre: int):
